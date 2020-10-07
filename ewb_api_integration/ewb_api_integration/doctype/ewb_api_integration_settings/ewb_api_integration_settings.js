c0273761db9dcedf08d06f792944acbd9ca834c0
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2020-10-07 10:16:16 +0530

    Added childtable gstin credential mapping, gsp list in json, depends on gsp condition removed

--- ewb_api_integration/ewb_api_integration/doctype/ewb_api_integration_settings/ewb_api_integration_settings.js
+++ ewb_api_integration/ewb_api_integration/doctype/ewb_api_integration_settings/ewb_api_integration_settings.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('EWB API Integration Settings', {
-	onload: function(frm) {
-		frm.set_df_property("gsp","options",['','Adaequare'].join('\n'))
-	}
+	// refresh: function(frm) {
+
+	// }
 });
